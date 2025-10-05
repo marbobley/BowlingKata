d11bff543d5cfd324645e2f22d723d70d872004a
Marbobley <fournier.wilf@gmail.com>
2025-10-05 19:52:51 +0200

    TDD DONE

--- src/BowlingScoreCalculator.php
+++ src/BowlingScoreCalculator.php
@@ -16,25 +16,35 @@ class BowlingScoreCalculator
         }
 
         $currentIndex = 0;
-        for($i = 0; $i < count($array); $i++){
-            if(array_key_exists($i+1, $array))
+        for($i = 0; $i < count($array); $i = $i + 2){
+            if($array[$i]== 10 && array_key_exists($i+2, $array))
             {
-                if($array[$i] + $array[$i+1] == 10){
-                    if(array_key_exists($i+2, $array))
-                    {
-                        $scoreTotal += $array[$i] + $array[$i+1] + $array[$i+2];
-                    }else
-                    {
-                        $scoreTotal += $array[$i] + $array[$i+1];
-                    }
-                    $i++;
+                if($array[$i+2] == 10 && array_key_exists($i+4, $array))
+                {
+                    $scoreTotal += $array[$i] + $array[$i+2] + $array[$i+4];
                 }
-                else{
-                    $scoreTotal += $array[$i];
+                else
+                {
+                    if(array_key_exists($i+3, $array))
+                        $scoreTotal += $array[$i] + $array[$i+2] + $array[$i+3];
+                    else
+                        $scoreTotal += $array[$i] + $array[$i+2];
+                }
+            }
+            else if(array_key_exists($i+1, $array) && ($array[$i] + $array[$i+1] == 10)  ){
+                if(array_key_exists($i+2, $array))
+                {
+                    $scoreTotal += $array[$i] + $array[$i+1] + $array[$i+2];
+                }else
+                {
+                    $scoreTotal += $array[$i] + $array[$i+1];
                 }
             }
             else{
-                $scoreTotal += $array[$i];
+                if(array_key_exists($i+1, $array))
+                    $scoreTotal += $array[$i] + $array[$i+1];
+                else
+                    $scoreTotal += $array[$i];
             }
         }
 
