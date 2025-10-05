092f9d34875eaa1c9d1069fb3a80b3c912490fc0
Marbobley <fournier.wilf@gmail.com>
2025-10-05 20:13:14 +0200

    just to try it

--- src/index.php
+++ src/index.php
@@ -1,3 +1,6 @@
 <?php
 
-echo 'hello';
+require __DIR__ . "/../src/BowlingScoreCalculator.php";
+$scoreBOw = new BowlingScoreCalculator();
+$score = $scoreBOw->getBowlingScore([10,0,10,0,10,0,10,0,10,0,10,0,10,0,10,0,10,0,10,10,10]);
+echo $score;
