e9b5ba0dbb3d6d59abb447531cb49171f46512ff
Marbobley <fournier.wilf@gmail.com>
2025-10-05 20:07:46 +0200

    refacto playing

--- src/BowlingScoreCalculator.php
+++ src/BowlingScoreCalculator.php
@@ -15,39 +15,70 @@ class BowlingScoreCalculator
             return $array[0];
         }
 
-        $currentIndex = 0;
         for($i = 0; $i < count($array); $i = $i + 2){
-            if($array[$i]== 10 && array_key_exists($i+2, $array))
+            $currentScoreLancer1 = $array[$i];
+            if($this->isItStrike($currentScoreLancer1) && array_key_exists($i+2, $array))
             {
-                if($array[$i+2] == 10 && array_key_exists($i+4, $array))
+                if($this->isItStrike($array[$i + 2]) && array_key_exists($i+4, $array))
                 {
-                    $scoreTotal += $array[$i] + $array[$i+2] + $array[$i+4];
+                    $scoreTotal += $currentScoreLancer1 + $array[$i+2] + $array[$i+4];
                 }
                 else
                 {
                     if(array_key_exists($i+3, $array))
-                        $scoreTotal += $array[$i] + $array[$i+2] + $array[$i+3];
+                        $scoreTotal += $currentScoreLancer1 + $array[$i+2] + $array[$i+3];
                     else
-                        $scoreTotal += $array[$i] + $array[$i+2];
+                        $scoreTotal += $currentScoreLancer1 + $array[$i+2];
                 }
             }
-            else if(array_key_exists($i+1, $array) && ($array[$i] + $array[$i+1] == 10)  ){
+            else if(array_key_exists($i+1, $array) && $this->isItSpare($currentScoreLancer1, $array[$i+1]))
+            {
                 if(array_key_exists($i+2, $array))
                 {
-                    $scoreTotal += $array[$i] + $array[$i+1] + $array[$i+2];
+                    $scoreTotal += $this->SommeFrame($currentScoreLancer1, $array[$i+1]) + $array[$i+2];
                 }else
                 {
-                    $scoreTotal += $array[$i] + $array[$i+1];
+                    $scoreTotal += $this->SommeFrame($currentScoreLancer1, $array[$i+1]);
                 }
             }
-            else{
+            else
+            {
                 if(array_key_exists($i+1, $array))
-                    $scoreTotal += $array[$i] + $array[$i+1];
+                    $scoreTotal += $this->SommeFrame($currentScoreLancer1, $array[$i+1]);
                 else
-                    $scoreTotal += $array[$i];
+                    $scoreTotal += $currentScoreLancer1;
             }
         }
 
         return $scoreTotal;
+    }
+
+    /**
+     * @param $array
+     * @return bool
+     */
+    public function isItStrike($score): bool
+    {
+        return $score == 10;
+    }
+
+    /**
+     * @param array $array
+     * @param mixed $i
+     * @return bool
+     */
+    public function isItSpare(int $scoreLancer1, int $scoreLancer2): bool
+    {
+        return ($scoreLancer1 + $scoreLancer2 == 10);
+    }
+
+    /**
+     * @param array $array
+     * @param mixed $i
+     * @return mixed
+     */
+    public function SommeFrame(int $scoreLancer1, int $scoreLancer2): mixed
+    {
+        return $scoreLancer1 + $scoreLancer2;
     }
 }
